1f7b5e790f3f718962b9d74b8da76da3496af389
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-02-04 16:29:35 +0000

    Task 1B - second attempt

--- Task1B.py
+++ Task1B.py
@@ -1,4 +1,3 @@
-from turtle import st
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
@@ -11,6 +10,9 @@ def ten_sorted(stations,p,closest = True):
     if closest == True: return x[:10]
     else: return x[-10:]
 
-print(ten_sorted(stations,coords)) # prints closest 10 stations
-print(ten_sorted(stations,coords, closest= False)) # prints farthest 10 stations
-
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***\n")
+    print("Closest stations: \n")
+    print(ten_sorted(stations,coords),"\n") # prints closest 10 stations
+    print("Farthest stations: \n")
+    print(ten_sorted(stations,coords, closest= False)) # prints farthest 10 stations    
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,24 +5,16 @@
 geographical data.
 
 """
-import haversine
-from floodsystem.stationdata import build_station_list
+from distutils.command.build_clib import build_clib
+from haversine import haversine
+from .stationdata import build_station_list
 from .utils import sorted_by_key  # noqa
 
 def stations_by_distance(stations, p):
     
     distance_list = [] # initialised list for storing distances and names (in tuple form)
-    for station in stations: # iterating through each item in the stations list
-        name = station.name # storing the name of the station
-        for coord2 in station.coord: # iterating over the coordinates 
-            distance_list.append((name,haversine(coord2,p))) # haversine function computes the distance between two coordinates on earth
     
+    for station in stations: # iterating through each item in the stations list
+        distance_list.append((station.name,haversine(station.coord,p))) # haversine function computes the distance between two coordinates on earth   
     return sorted_by_key(distance_list,1) # sort by 2nd entry in tuple 
-
-
-         
-
-
-
-
-
+    
