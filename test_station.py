a8fffd55d680d768499b15178237e726c5fe3d54
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-02-07 21:24:31 +0000

    added tests for 1B and 1B

--- test_station.py
+++ test_station.py
@@ -45,21 +45,28 @@ def test_stations_by_distance():
     x = stations_by_distance(test_list,p) # does the function 
     d = [lis[1] for lis in x] # takes the 2nd element from the tuple
 
-    #assert d <= 1e-4 # tolerance is kept a little high because i am not sure how haversine numerically computes
+    assert d <= 1e-4 # tolerance is kept a little high because i am not sure how haversine numerically computes
 
 def test_station_within_radius(): 
 
     # Create a station
+    stations = [] # empty stations list
+    
     s_id = "test-s-id"
     m_id = "test-m-id"
     label = "some station"
-    coord = (-2.0, 4.0)
+    coord = (1, 2)
     trange = (-2.3, 3.4445)
     river = "River X"
     town = "My Town"
     s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
-    r = 0 
+    stations.append(s)
+    
+    p = (2, 1) # we know that this is < 200 km away from (1,2)
+    return_list = stations_within_radius(stations, p, 200)
+    
+    assert (return_list[0] == s)
 
 
 def test_typical_range_stations(): 
@@ -95,7 +102,4 @@ def test_typical_range_stations():
     inconsistent_stations = [k,j]
     assert inconsistent_typical_range_stations(stations) == inconsistent_stations
     
-    #!!! need to fix the test for the test_stations_by_distance and need to add this test (within radius check)
-    # add tests for typical_range_consistent - 1F
-    # add tests for inconsistent list - 1F
     
